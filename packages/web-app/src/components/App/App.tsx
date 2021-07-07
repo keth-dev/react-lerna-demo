ed5fb75c8e27d9f12da0860cafec3359e440b97f
keth-dev <77727801+keth-dev@users.noreply.github.com>
2021-07-07 22:40:42 +0200

    trigger changes

--- packages/web-app/src/components/App/App.tsx
+++ packages/web-app/src/components/App/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "@keth-dev/lerna-demo-shared-ui";
 export const App = () => {
   return (
     <div>
-      Hello World!! <Button label={"Button 2"} />
+      Hello World!! <Button label={"Button 3"} />
     </div>
   );
 };
