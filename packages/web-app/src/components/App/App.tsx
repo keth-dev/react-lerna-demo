aaf1c96c7b92cd2a7651091726902c72ca45082a
keth-dev <77727801+keth-dev@users.noreply.github.com>
2021-07-11 13:59:50 +0200

    feat: drop something
    BREAKING CHANGE: refactor to use something

--- packages/web-app/src/components/App/App.tsx
+++ packages/web-app/src/components/App/App.tsx
@@ -8,7 +8,7 @@ export const App = () => {
       onCreateAccount={console.log}
       onLogout={console.log}
     >
-      Hello World from Web App 11
+      Hello World from Web App 12
     </Page>
   );
 };
